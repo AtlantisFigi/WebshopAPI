46d65b855d1306721e4248828520641a0846bbfd
Jonas Everaard <150266686+AtlantisFigi@users.noreply.github.com>
2024-11-01 14:28:36 +0100

    .

--- src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
+++ src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.webshopapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 class WebshopApiApplicationTests {
 
     @Test
