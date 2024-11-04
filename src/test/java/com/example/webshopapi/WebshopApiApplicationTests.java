f15b43e756352b9eb4a62f0df0c18707ecbd3333
Jonas Everaard <150266686+AtlantisFigi@users.noreply.github.com>
2024-11-04 10:46:42 +0100

    .

--- src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
+++ src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
 class WebshopApiApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
