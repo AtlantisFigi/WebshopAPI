00285943b5ceb794064414ff526e6675a2022826
Jonas Everaard <150266686+AtlantisFigi@users.noreply.github.com>
2024-11-04 11:05:39 +0100

    .

--- src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
+++ src/test/java/com/example/webshopapi/WebshopApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.webshopapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebshopApiApplicationTests {
 
     @Test
